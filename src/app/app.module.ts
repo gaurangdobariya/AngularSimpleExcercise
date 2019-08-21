9da6a1901a5c04ab2a5ed79833fd0704771afc3c
Gaurang <dobariyagaurang@gmail.com>
2019-08-21 19:15:56 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { BaseModule } from './base/base.module';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CollapseModule,
+    BaseModule,
+    BrowserAnimationsModule
+    // CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
